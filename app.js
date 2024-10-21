dd78fd81edca5e7320650891b4661cd22750307e
mohammadhhhhhh <9025722@student.zadkine.nl>
2024-10-21 15:44:09 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+let getal = 0;
+let output = document.getElementById ("output");
+
+function addnumber(){
+
+
+
+    if(getal >= 10){
+        getal = 0;
+    } else {
+        getal++
+    }
+    
+    
+    output.innerText = getal
+    }
+
+
+
+
+
